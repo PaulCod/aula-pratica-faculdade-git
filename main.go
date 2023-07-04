50de7b5c761ca111712d9703db3e004ba20611ce
Paulo Jr <paulods086543@gmail.com>
2023-07-04 19:37:13 -0300

    Adicionando Struct Person

--- main.go
+++ main.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 
+type Person struct {
+	name  string
+	email string
+}
+
+func (p *Person) getdata() {
+	fmt.Printf("nome: %s, email: %s\n", p.name, p.email)
+}
+
 func main() {
-	fmt.Println("Hello world")
+	var name, email string
+
+	fmt.Println("Escreva seu nome: ")
+	fmt.Scanf("%s\n", &name)
+
+	fmt.Println("escreva seu email: ")
+	fmt.Scanf("%s\n", &email)
+
+	p := Person{name: name, email: email}
+	p.getdata()
 }
